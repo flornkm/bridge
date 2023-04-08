a5f3f68c6b790f2585f65570d9854551cca900ff
flooorianflo <florian.kiem@hfg.design>
2023-04-08 15:13:01 +0200

    Applications are now showed

--- src/pages/[id].js
+++ src/pages/[id].js
@@ -30,7 +30,7 @@ export default function Published() {
     return (
         <>
             <Head>
-                <title>Bridge - Effiecient hiring process with joy</title>
+                <title>{id && id.split('&')[1].replace(/-/g, ' ').replace(/\b\w/g, l => l.toUpperCase())}</title>
                 <meta
                     name="description"
                     content="Bridge is a tool that allows you to create interactive elements for your website."
@@ -63,20 +63,38 @@ export default function Published() {
             </Head>
 
             <main className="h-full w-full bg-white">
-                <div className="max-md:w-[90%] w-full max-w-7xl pl-[5%] pr-[5%] m-auto min-h-[200vh] bg-white">
-                    {data && (
-                        data.content.map((items, index) => {
-                            return (
-                                items.content.map((item, index) => {
-                                    return (
-                                        <div key={item.id} className="text-black textt-lg">
-                                            <p>{item.content}</p>
-                                        </div>
-                                    )
-                                })
-                            )
-                        })
-                    )}
+                <div className="max-md:w-[90%] w-full max-w-7xl pl-[5%] pr-[5%] m-auto min-h-[200vh] bg-white pt-24">
+                    <div className="flex flex-col gap-4">
+                        {data && (
+                            data.content.map((items, index) => {
+                                return (
+                                    items.content.map((item, index) => {
+                                        if (item.type === "heading" && item.visibility) {
+                                            return (
+                                                <h1 key={index} className="text-4xl font-semibold" style={{ color: data.colors.heading }}>{item.content}</h1>
+                                            )
+                                        } else if (item.type === "text" && item.visibility) {
+                                            return (
+                                                <p key={index} className="text-lg mb-8" style={{ color: data.colors.text }}>{item.content}</p>
+                                            )
+                                        } else if (item.type === "textInput" && item.visibility) {
+                                            return (
+                                                <div key={index} className="flex flex-col gap-1">
+                                                    <div className="flex gap-1 items-center">
+                                                        <label className="text-base" style={{ color: data.colors.label }}>{item.label}</label>
+                                                        {item.required && <span className="text-red-500">*</span>}
+                                                    </div>
+                                                    <input required={item.required} className="ring-1 ring-gray-200 rounded-md px-4 py-3 w-full focus:outline-black focus:outline-1"
+                                                        style={{ color: data.colors.text, placeholder: { color: data.colors.text } }}
+                                                        placeholder={item.content} />
+                                                </div>
+                                            )
+                                        }
+                                    })
+                                )
+                            })
+                        )}
+                    </div>
                 </div>
             </main>
         </>
