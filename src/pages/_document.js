f0f00e0c0af58a0c0e0c32c0a6bcb1d8336f48f1
flooorianflo <florian.kiem@hfg.design>
2023-04-09 20:14:54 +0200

    Adjustments Landingpage

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-neutral-50">
+      <body>
         <Main />
         <NextScript />
       </body>
--- src/pages/index.js
+++ src/pages/index.js
@@ -187,22 +187,22 @@ export default function Home() {
             <main className="h-full w-full bg-white">
                 <div className="max-md:w-[90%] w-full max-w-7xl pl-[5%] pr-[5%] m-auto min-h-[200vh] bg-white">
                     <div
-                        className="min-h-64 pt-48 relative z-20"
+                        className="min-h-64 md:pt-48 max-md:pt-40 relative z-20 border-b border-b-neutral-200"
 
                     >
-                        <div className="max-w-3xl flex flex-col gap-6 mb-20">
-                            <h1 className="font-semibold text-5xl leading-tight">
+                        <div className="max-w-3xl flex flex-col gap-6 md:mb-20">
+                            <h1 className="font-semibold md:text-5xl max-md:text-3xl leading-tight">
                                 Streamline your hiring process
                                 with an interactive tool.
                             </h1>
-                            <p className="text-gray-500 font-medium text-3xl">
+                            <p className="text-gray-500 font-medium md:text-3xl max-md:text-xl leading-tight">
                                 Create a high-quality candidate experience and find
-                                the best talent for your business with bridge.
+                                the best talent for your business with Bridge.
                             </p>
                         </div>
-                        <div className="flex justify-between relative left-[50%] translate-x-[-50%] w-screen overflow-hidden py-10 pl-[5%] pr-[5%] items-center md:cursor-none" onMouseEnter={() => setCustomCursor(true)}
+                        <div className="flex justify-between relative left-[50%] translate-x-[-50%] w-screen overflow-hidden max-md:py-48 md:py-10 pl-[5%] pr-[5%] items-center md:cursor-none" onMouseEnter={() => setCustomCursor(true)}
                             onMouseLeave={() => setCustomCursor(false)}>
-                            <div className="bg-slate-900 text-slate-100 text-xl px-7 py-5 rounded-2xl -rotate-2 relative xl:left-32 max-xl:left-10 -top-6">
+                            <div className="bg-slate-900 text-slate-100 text-xl px-7 py-5 rounded-2xl -rotate-2 relative xl:left-32 max-xl:left-10 -top-6 max-md:hidden">
                                 <code className="font-mono">
                                     <pre>
                                         &#91;
@@ -223,10 +223,10 @@ export default function Home() {
                                     </pre>
                                 </code>
                             </div>
-                            <div className="py-4 px-8 bg-white z-20 shadow-2xl rounded-2xl ring-1 ring-neutral-200 absolute left-[50%] translate-x-[-50%] w-[90%] md:max-w-5xl flex justify-between max-lg:flex-col gap-10 transition-all duration-500 md:hover:scale-105">
-                                <div className="h-56" />
+                            <div className="py-4 px-8 bg-white z-20 shadow-2xl rounded-2xl ring-1 ring-neutral-200 top-24 absolute left-[50%] translate-x-[-50%] w-[90%] md:max-w-4xl flex justify-between max-lg:flex-col gap-10 transition-all duration-500 md:hover:scale-[1.01]">
+                                <div className="h-96" />
                             </div>
-                            <div className="bg-neutral-50 ring-1 ring-neutral-200 text-slate-100 text-xl px-7 py-4 rounded-2xl rotate-2 relative xl:right-32 max-xl:right-32 -top-16">
+                            <div className="bg-neutral-50 ring-1 ring-neutral-200 text-slate-100 text-xl px-7 py-4 rounded-2xl rotate-2 relative xl:right-32 max-xl:right-32 -top-16 max-md:hidden">
                                 <div className="p-4 rounded-full h-56 w-56 bg-gradient-to-r from-violet-500 to-purple-400 flex justify-center items-center">
                                     <div className="bg-white rounded-full h-44 w-44 p-4 flex justify-center items-center flex-col gap-1">
                                         <p className="font-bold text-5xl text-purple-900">100</p>
@@ -236,17 +236,17 @@ export default function Home() {
                             </div>
                         </div>
                     </div>
-                    <div className="bg-[url('/images/general/morph_lines.svg')] bg-top bg-no-repeat w-full h-[768px] absolute top-64 pointer-events-none left-[50%] translate-x-[-50%]">
+                    <div className="bg-[url('/images/general/morph_lines.svg')] bg-top bg-no-repeat w-full max-md:h-96 md:h-[768px] absolute top-64 pointer-events-none left-[50%] translate-x-[-50%]">
                         <div className="bg-gradient-to-t from-white via-30% via-transparent to-white absolute z-10 top-0 left-0 right-0 bottom-0" />
                     </div>
                     <div className="min-h-64 pt-24">
-                        <div className="w-full flex justify-center flex-col items-center gap-3 mb-16">
-                            <h2 className="font-semibold text-4xl text-black flex gap-4 items-center">Getting used to it is <Icon.FastForward weight="fill" className="text-violet-900" /></h2>
-                            <p className="text-gray-500 text-xl font-medium">Features helping you to implement your goals.</p>
+                        <div className="w-full flex justify-center flex-col md:items-center gap-3 mb-16">
+                            <h2 className="font-semibold md:text-4xl max-md:text-2xl text-black flex gap-4 items-center">Features that&apos;ll make you <Icon.FastForward weight="fill" className="text-violet-900" /></h2>
+                            <p className="text-gray-500 text-xl font-medium">Built around users and their needs</p>
                         </div>
                         <div className="grid grid-cols-3 gap-4 max-md:grid-cols-1">
                             <div className="flex flex-col gap-3 p-6 bg-neutral-50 rounded-2xl overflow-hidden relative h-96">
-                                <h3 className="text-black font-semibold text-2xl">Drag and Drop</h3>
+                                <h3 className="text-black font-semibold md:text-2xl max-md:text-xl">Drag and Drop</h3>
                                 <div className="flex flex-col gap-8 items-start bg-white left-8 p-8 absolute right-4 bottom-4 pr-8 ring-1 top-[30%] ring-neutral-200 rounded-2xl pb-8">
                                     <DndContext
                                         sensors={sensors}
@@ -274,13 +274,13 @@ export default function Home() {
                                 </div>
                             </div>
                             <div className="flex flex-col md:col-span-2 gap-3 p-6 bg-neutral-50 rounded-2xl overflow-hidden relative h-96">
-                                <h3 className="text-black font-semibold text-2xl">Made for the future</h3>
+                                <h3 className="text-black font-semibold md:text-2xl max-md:text-xl">Made for the future</h3>
                                 <div className="absolute left-16 top-[60%] translate-y-[-50%] h-48 w-full bg-no-repeat bg-right-center bg-[url('/images/general/privacy_badges.svg')]">
                                     <div className="absolute z-10 bg-gradient-to-r from-transparent to-neutral-100 top-0 bottom-0 left-0 right-0" />
                                 </div>
                             </div>
                             <div className="flex flex-col md:col-span-2 gap-3 p-6 bg-neutral-50 rounded-2xl overflow-hidden h-96">
-                                <h3 className="text-black font-semibold text-2xl flex gap-4 items-center">Connect with canditates
+                                <h3 className="text-black font-semibold md:text-2xl max-md:text-xl flex gap-4 items-center">Connect with canditates
                                     {/* <span className="rounded-full ring-2 ring-purple-300 text-purple-500 text-sm px-2 py-1">Soon</span> */}
                                 </h3>
                                 <div className="w-full flex justify-between h-full items-center gap-4">
@@ -292,7 +292,7 @@ export default function Home() {
                                 </div>
                             </div>
                             <div className="flex flex-col gap-3 p-6 bg-neutral-50 rounded-2xl overflow-hidden h-96">
-                                <h3 className="text-black font-semibold text-2xl">Publishing made easy</h3>
+                                <h3 className="text-black font-semibold md:text-2xl max-md:text-xl">Publishing made easy</h3>
                                 <div className="h-full w-full flex justify-center items-center">
                                     <button onClick={() => {
                                         setConfetti(true)
@@ -310,7 +310,7 @@ export default function Home() {
                     </div>
                     <div className="pt-40 pb-40">
                         <div className="flex flex-col gap-3 mb-10">
-                            <h2 className="font-semibold text-4xl text-black flex gap-4 items-center">An intuitive Editor </h2>
+                            <h2 className="font-semibold md:text-4xl max-md:text-2xl text-black flex gap-4 items-center">An intuitive Editor </h2>
                             <p className="text-gray-500 text-xl font-medium">Our editor works for everyone.</p>
                         </div>
                         {isInView ? (
@@ -323,30 +323,32 @@ export default function Home() {
                     </div>
                     <div className="pb-56">
                         <div className="flex flex-col gap-3 mb-10">
-                            <h2 className="font-semibold text-4xl text-black flex gap-4 items-center">Why tho?</h2>
+                            <h2 className="font-semibold md:text-4xl max-md:text-2xl text-black flex gap-4 items-center">Why Bridge when there are a lot of other tools?</h2>
                         </div>
                         <div className="">
 
                         </div>
                     </div>
-                    <div className="p-[1px] rounded-3xl relative overflow-hidden">
-                        <div className="pt-16 px-8 bg-indigo-50 rounded-[23px] relative z-20">
+                    <div className="p-[1px] rounded-3xl bg-indigo-100 relative overflow-hidden">
+                        <div className="pt-16 px-8 bg-white bg-opacity-90 rounded-[23px] relative z-20">
                             <div className="flex flex-col gap-3">
-                                <h2 className="font-semibold text-3xl text-indigo-500 flex gap-4 items-center justify-center">Integrate with your favourite tools</h2>
+                                <h2 className="font-semibold md:text-3xl max-md:text-2xl text-indigo-500 flex gap-4 items-center justify-center">Integrate in your Pipeline</h2>
                             </div>
-                            <RiveComponent src="/animations/bridge_explanation.riv" className="w-full aspect-video" />
+                            <div className="max-md:h-80 w-full flex justify-center">
+                                <RiveComponent src="/animations/bridge_explanation.riv" className="max-md:w-screen md:w-[85%] max-md:absolute max-md:translate-x-[-50%] left-[50%] aspect-video" />
+                            </div>
                         </div>
                         <div className="absolute top-0 bottom-0 right-0 left-0 bg-indigo-400 animate-[spin_10s_linear_infinite] blur-2xl z-10" />
                     </div>
                     <div className="h-56" />
                 </div>
-
             </main>
             {customCursor && <Image
+                priority
                 src="/images/general/custom_cursor.svg"
                 alt="Custom Bridge Cursor"
-                width={128}
-                height={128}
+                width={96}
+                height={96}
                 ref={cursor}
                 className={
                     "fixed z-50 -translate-x-10 -translate-y-8 pointer-events-none max-md:hidden"
