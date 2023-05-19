7eea454408adc9118254940dd2a077ec676abe69
flooorianflo <florian.kiem@hfg.design>
2023-05-19 18:35:12 +0200

    Video Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -619,7 +619,7 @@ export default function Home() {
                         </div>
                         {isInView ? (
                             <video autoPlay loop muted className="rounded-2xl ring-1 w-full object-cover aspect-video bg-[#FAF9FA] ring-zinc-200">
-                                <source src="/videos/showcase.mp4" type="video/mp4" />
+                                <source src="/videos/tool_showcase.mp4" type="video/mp4" />
                             </video>
                         ) : (
                             <div ref={ref}></div>
